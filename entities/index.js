ea92c08d365faeb035cd47ab4edc937d8fe2c07c
summerroh <summerrohh@gmail.com>
2022-02-18 17:59:50 +1100

    commit

--- entities/index.js
+++ entities/index.js
@@ -1,10 +1,12 @@
 import Matter from "matter-js"
 import Bird from "../components/Bird";
 import Floor from "../components/Floor";
+import Obstacle from "../components/Obstacle";
 
 import { Dimensions } from "react-native";
+import { getPipeSizePosPair } from "../utils/random";
 
-const windowHeights = Dimensions.get('window').height
+const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
 
 export default restart => {
@@ -14,9 +16,19 @@ export default restart => {
 
   engine.gravity.y = 0.4;
 
+  const pipeSizePosA = getPipeSizePosPair(-100)
+  const pipeSizePosB = getPipeSizePosPair(windowWidth * 0.9)
+
   return {
     physics: {engine, world},
     Bird: Bird(world, 'green', { x: 50, y: 300 }, { height: 40, width: 40 }),
-    Floor: Floor(world, 'blue', { x: windowWidth / 2, y: windowHeights }, { height: 50, width: windowWidth }),
+
+    ObstacleTop1: Obstacle(world, 'ObstacleTop1', 'red', pipeSizePosA.pipeTop.pos, pipeSizePosA.pipeTop.size),
+    ObstacleBottom1: Obstacle(world, 'ObstacleBottom1', 'black', pipeSizePosA.pipeBottom.pos, pipeSizePosA.pipeBottom.size),
+
+    ObstacleTop2: Obstacle(world, 'ObstacleTop2', 'red', pipeSizePosB.pipeTop.pos, pipeSizePosB.pipeTop.size),
+    ObstacleBottom2: Obstacle(world, 'ObstacleBottom2', 'black', pipeSizePosB.pipeBottom.pos, pipeSizePosB.pipeBottom.size),
+
+    Floor: Floor(world, 'blue', { x: windowWidth / 2, y: windowHeight }, { height: 50, width: windowWidth }),
   }
 }
--- physics.js
+++ physics.js
@@ -12,6 +12,11 @@ const Physics = ( entities , { touches, time, dispatch }) => {
   })
 
   Matter.Engine.update( engine, time.delta )
+  for (let index = 1; index <= 2; index++) {
+    Matter.Body.translate( entities['ObstacleTop1${index}'].body, {x: -2, y: 0})
+    Matter.Body.translate( entities['ObstacleBottom1'].body, {x: -2, y: 0})
+    // Matter.Body.rotate( entities['ObstacleTop1'].body, 1)
+  }
 
   return entities;
 }
